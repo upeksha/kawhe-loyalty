8df71c72b6a6d03d2a9313bbc586b95c011de172
Isuru <upekshamendiz@gmail.com>
2026-01-16 09:29:06 +1300

    Fix passgenerator config to match package's expected structure

--- config/passgenerator.php
+++ config/passgenerator.php
@@ -7,13 +7,20 @@ return [
     |--------------------------------------------------------------------------
     |
     | Configuration for generating Apple Wallet passes (.pkpass files)
+    | This config matches the byte5/laravel-passgenerator package expectations
     |
     */
 
-    'certificate_path' => env('CERTIFICATE_PATH', 'passgenerator/certs/certificate.p12'),
-    'certificate_pass' => env('CERTIFICATE_PASS', ''),
-    'wwdr_certificate' => env('WWDR_CERTIFICATE', 'passgenerator/certs/AppleWWDRCA.pem'),
-    'pass_type_identifier' => env('APPLE_PASS_TYPE_IDENTIFIER', 'pass.com.kawhe.loyalty'),
-    'team_identifier' => env('APPLE_TEAM_IDENTIFIER', ''),
-    'organization_name' => env('APPLE_ORGANIZATION_NAME', 'Kawhe'),
+    'config_disk' => env('PASSGENERATOR_CONFIG_DISK'), // The disk to use for storing the pass configuration files and certificates
+
+    'certificate_store_path' => env('CERTIFICATE_PATH', 'passgenerator/certs/certificate.p12'), // The path to the certificate store (a valid PKCS#12 file)
+    'certificate_store_password' => env('CERTIFICATE_PASS', ''), // The password to unlock the certificate store
+    'wwdr_certificate_path' => env('WWDR_CERTIFICATE', 'passgenerator/certs/AppleWWDRCA.pem'), // Get from here https://www.apple.com/certificateauthority/ and export to PEM
+
+    'storage_disk' => env('PASSGENERATOR_STORAGE_DISK', 'local'), // The disk to use for storing the pass files
+    'storage_path' => env('PASSGENERATOR_STORAGE_PATH', 'passgenerator/passes'), // The path to store the pass files on the disk
+
+    'pass_type_identifier' => env('APPLE_PASS_TYPE_IDENTIFIER', env('PASS_TYPE_IDENTIFIER', 'pass.com.kawhe.loyalty')),
+    'organization_name' => env('APPLE_ORGANIZATION_NAME', env('ORGANIZATION_NAME', 'Kawhe')),
+    'team_identifier' => env('APPLE_TEAM_IDENTIFIER', env('TEAM_IDENTIFIER', '')),
 ];
