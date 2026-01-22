def756279e45885da8af1cc7c21d3d4fd8fa09ca
Isuru <upekshamendiz@gmail.com>
2026-01-23 09:28:50 +1300

    Fix dropdown direction - properly apply origin classes for upward opening
    - Fix alignment classes to use origin-bottom when direction is up
    - Ensure position classes (bottom-full/top-full) are correctly applied
    - Fix origin classes to match direction (bottom-left/bottom-right for up, top-left/top-right for down)

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -1,21 +1,29 @@
 @props(['align' => 'right', 'width' => '48', 'contentClasses' => 'py-1 bg-white border border-stone-200', 'direction' => 'down'])
 
 @php
-$alignmentClasses = match ($align) {
-    'left' => 'ltr:origin-top-left rtl:origin-top-right start-0',
-    'top' => 'origin-top',
-    default => 'ltr:origin-top-right rtl:origin-top-left end-0',
-};
-
 $width = match ($width) {
     '48' => 'w-48',
     default => $width,
 };
 
-$directionClasses = match ($direction) {
-    'up' => 'bottom-full mb-2 origin-bottom',
-    default => 'top-full mt-2 origin-top',
-};
+// Build alignment and direction classes together
+if ($direction === 'up') {
+    // Opening upward
+    $positionClasses = 'bottom-full mb-2';
+    $alignmentClasses = match ($align) {
+        'left' => 'ltr:origin-bottom-left rtl:origin-bottom-right start-0',
+        'top' => 'origin-bottom',
+        default => 'ltr:origin-bottom-right rtl:origin-bottom-left end-0',
+    };
+} else {
+    // Opening downward (default)
+    $positionClasses = 'top-full mt-2';
+    $alignmentClasses = match ($align) {
+        'left' => 'ltr:origin-top-left rtl:origin-top-right start-0',
+        'top' => 'origin-top',
+        default => 'ltr:origin-top-right rtl:origin-top-left end-0',
+    };
+}
 @endphp
 
 <div class="relative" x-data="{ open: false }" @click.outside="open = false" @close.stop="open = false">
@@ -30,7 +38,7 @@ $directionClasses = match ($direction) {
             x-transition:leave="transition ease-in duration-75"
             x-transition:leave-start="opacity-100 scale-100"
             x-transition:leave-end="opacity-0 scale-95"
-            class="absolute z-50 {{ $directionClasses }} {{ $width }} rounded-md shadow-lg {{ $alignmentClasses }}"
+            class="absolute z-50 {{ $positionClasses }} {{ $width }} rounded-md shadow-lg {{ $alignmentClasses }}"
             style="display: none;"
             @click="open = false">
         <div class="rounded-md ring-1 ring-stone-200 ring-opacity-50 shadow-lg {{ $contentClasses }}">
