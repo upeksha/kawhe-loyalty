dc26c84db5e47a8d1709e9b1bb4c4ba97cceb3fb
Isuru <upekshamendiz@gmail.com>
2026-02-28 09:39:42 +1300

    Expose store branding metadata to merchant app API

--- app/Http/Controllers/Api/StoreController.php
+++ app/Http/Controllers/Api/StoreController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class StoreController extends Controller
 {
@@ -17,9 +18,22 @@ class StoreController extends Controller
                 'reward_target',
                 'reward_title',
                 'require_verification_for_redemption',
+                'brand_color',
+                'background_color',
+                'logo_path',
+                'pass_logo_path',
             ])
             ->orderBy('name')
             ->get();
+
+        $stores->transform(function ($store) {
+            $logoPath = $store->logo_path ?: $store->pass_logo_path;
+            $store->logo_url = $logoPath
+                ? url(Storage::disk('public')->url($logoPath))
+                : null;
+
+            return $store;
+        });
 
         return response()->json([
             'stores' => $stores,
